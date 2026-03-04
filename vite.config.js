2dc6f9937d5d9ad1192270fccd1daee9a05f7466
Karun Katepally <carunreddy05@gmail.com>
2026-03-03 19:18:25 -0500

    config file updates

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react(),
-            tailwindcss(),
-            tsconfigPaths(),
-          ],
+  resolve: {
+    alias: {
+      // explicit path mapping for `@` to ensure Vite always resolves
+      // correctly even if tsconfigPaths plugin has issues.
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+  ],
   build: {
     target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14', 'ios12'],
     outDir: 'dist',
